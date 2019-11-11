48a4850176a2162b72fde90a97a4a31f8e280983
Alex Woods <alex.woods89@hotmail.co.uk>
2019-11-11 13:19:39 +0000

    cover letter maker v1

---
+++ cover_letter_maker.rb
@@ -0,0 +1,19 @@
+company = "Tesco Bank"
+job_title = "Cloud Engineer"
+company_tech_stack = ".NET and C#"
+my_name = "Alex Woods"
+
+puts "
+
+Dear Hiring Manager,
+
+I am writing today to submit my application for the #{job_title} role that is advertised on your website.
+
+I believe that I would be a great addition to #{company} as I am a highly motivated individual with a desire to learn, the benefits that #{company} offers its employees are highly appealing to me as I have a thirst for knowledge, so the support to continuously learn and develop is important to me Also after looking at your website I felt that your product was really cool and that it could actually help lots of people.
+In terms of the tech stack you work with, I am comfortable with JavaScript, Ruby and Java. I am also familiar with the principles of of OOP and SOLID and have experience of working with XML, JSON and API’S on the multiple projects I have taken part in.I would love the opportunity to learn #{company_tech_stack} technologies as expanding my repertoire of programming languages is a goal of mine, especially as by developing my skills at #{company} it would allow me to learn from those around me. As a company, your company provides an exciting client base to work with but also the rare opportunity to do work which is meaningful and impacts on peoples lives. 
+While studying at CodeClan I spent at least 800 hours coding over the 16 weeks course and also achieve an SCQF level 8 Professional Development Award in Software Development. I will also be gaining industry experience by volunteering with Coder Dojo and working with charities in the Edinburgh area to help them develop software. The challenge of working with complex problems is one that I thoroughly enjoy and embrace, as I find coming up with creative solutions to be extremely satisfying and fulfilling. I am flexible in my approach to solving such issues as I relish working in a team environment, but am also capable independently. Additionally I have several years of experience working for both small and large companies, where I have been fortunate enough to take on multiple roles. This has provided me with a varied skill set, especially the ability to liaise with customers and stakeholders. 
+I look forward to hearing from you soon.
+Yours Sincerely,
+#{my_name}
+
+"
